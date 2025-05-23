7b10adb4cae5210befabbf3550bafa49d26602e2
Jayv2017 <75362566+Jayv2017@users.noreply.github.com>
2025-05-23 21:44:27 +0530

    added the gateway image

--- src/assets/Hero.svelte
+++ src/assets/Hero.svelte
@@ -1,17 +1,15 @@
-<!-- hero and what is vexos section -->
-<div class="relative overflow-hidden bg-dark pb-64"> <!-- Added pb-64 to create space for the flame -->
-  <div class="max-w-7xl mx-auto pt-48 px-4 sm:px-6 lg:px-8">
-
+<!-- combined hero and what is vexos section -->
+<div class="relative overflow-hidden bg-dark pb-64">
+  <div class="max-w-7xl mx-auto lg:pt-48 md:pt-48 pt-24 px-4 sm:px-6 lg:px-8">
       <!-- grid -->
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 pb-12">
-
           <!-- left half text -->
           <div class="flex flex-col justify-end h-full order-2 md:order-1 ">
             <p class="text-light text-lg ">A single platform that tells you exactly where to place what matters — whether it's a store, a sensor, or a security team.</p>
           </div>
 
-           <!-- right half text -->
-           <h1 class="text-light font-medium text-6xl md:text-6xl lg:text-[90px] text-right md:text-right order-1 md:order-2"> RE-IMAGINING LOCATION DECISIONS</h1>
+          <!-- right half text -->
+          <h1 class="text-light font-medium text-6xl md:text-6xl lg:text-[90px] text-right md:text-right order-1 md:order-2"> RE-IMAGINING LOCATION DECISIONS</h1>
       </div>
 
       <!-- horizontal line -->
@@ -19,5 +17,21 @@
   </div>
 
   <!-- flame 1 -->
-  <img class="absolute right-0 md:-mt-52 lg:-mt-80 -mt-28 z-10" src="/flame-1.png">
+  <img class="absolute right-0 md:-mt-52 lg:-mt-96 -mt-28 z-10" src="/flame-1.png">
+
+  <!-- what is vexos image section -->
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-20 lg:mt-24 mt-16">
+    <div class="relative">
+      <!-- laptop image -->
+      <img src="/gateway-image.png" alt="Gateway Image" class="w-full h-auto hidden sm:block">
+
+      <!-- mobile size image -->
+      <img src="/mobile-gateway-image.png" alt="Gateway Image" class="w-full h-auto block sm:hidden">
+
+      <!-- what is vexos text over the image -->
+      <div class="absolute inset-0 flex items-center">
+        <h1 class="text-light font-medium text-5xl pl-6">What is Vexos?</h1>
+      </div>
+    </div>
+  </div>
 </div>
